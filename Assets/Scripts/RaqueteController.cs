6a0159acc17bc8f7f484a70b06827548ecf5c183
Isabelle <isasoarez21@gmail.com>
2025-08-12 18:25:31 -0300

    Adiciona controle de movimento à RaqueteController

--- Assets/Scripts/RaqueteController.cs
+++ Assets/Scripts/RaqueteController.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class RaqueteController : MonoBehaviour
 {
     // criando o meu vector 3 - classe que representa um ponto no espaço 3D
-    public Vector3 minhapos;
+    private Vector3 minhapos;
     public float  meuEixoy;
-    
+    public float velocidade = 5f;
+
     void Start()
     {
         minhapos = transform.position; // pega a posição atual do objeto
@@ -21,6 +22,19 @@ public class RaqueteController : MonoBehaviour
 
         //modificar a posição da raquete
         transform.position = minhapos;
+
+
+        //pegando a setinha para cima 
+        if (Input.GetKey(KeyCode.UpArrow)){ 
+            //aumenta valor do meu y
+            meuEixoy = meuEixoy + velocidade * Time.deltaTime;
+
+        };
+
+        if(Input.GetKey(KeyCode.DownArrow)){
+            //diminuir o valor do meu y
+            meuEixoy = meuEixoy - velocidade * Time.deltaTime;
+        };
 
     }
 }
