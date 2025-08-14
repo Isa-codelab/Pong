8973b2f9e4bac3d58da08e9bfa88bdbffb7bb8b3
Isabelle <isasoarez21@gmail.com>
2025-08-14 14:20:56 -0300

    feat: adiciona delay antes do jogo iniciar

--- Assets/Scripts/BolaController.cs
+++ Assets/Scripts/BolaController.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class BolaController : MonoBehaviour
 {
+    public bool jogoiniciado = false;
+
+    public float delay = 2f;
+
     public Transform transformCamera;
 
     public AudioClip somColisao;
@@ -16,41 +20,46 @@ public class BolaController : MonoBehaviour
     public float velocidade = 5f;
 
     public float limiteHorizontal = 12f;
-    // Start is called before the first frame update
+   
     void Start()
     {
-        minhaVelocidade.x = velocidade;
-        minhaVelocidade.y = velocidade;
-
-        rb.velocity = minhaVelocidade;
-
-        int direcao = Random.Range(0, 4);
-
-        switch(direcao)
-        {
-            case 0:
-                minhaVelocidade.x = velocidade;
-                minhaVelocidade.y = velocidade;
-                break;
-            case 1:
-                minhaVelocidade.x = -velocidade;
-                minhaVelocidade.y = velocidade;
-                break;
-            case 2:
-                minhaVelocidade.x = velocidade;
-                minhaVelocidade.y = -velocidade;
-                break;
-            case 3:
-                minhaVelocidade.x = -velocidade;
-                minhaVelocidade.y = -velocidade;
-                break;
-        }
-
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //iniciar a bola com delay
+        delay = delay - Time.deltaTime;
+
+        if(delay <= 0 || jogoiniciado == false)
+        {
+            jogoiniciado = true;
+
+            int direcao = Random.Range(0, 4);
+
+            switch (direcao)
+            {
+                case 0:
+                    minhaVelocidade.x = velocidade;
+                    minhaVelocidade.y = velocidade;
+                    break;
+                case 1:
+                    minhaVelocidade.x = -velocidade;
+                    minhaVelocidade.y = velocidade;
+                    break;
+                case 2:
+                    minhaVelocidade.x = velocidade;
+                    minhaVelocidade.y = -velocidade;
+                    break;
+                case 3:
+                    minhaVelocidade.x = -velocidade;
+                    minhaVelocidade.y = -velocidade;
+                    break;
+            }
+
+            rb.velocity = minhaVelocidade;
+        }
+
+
         //checando se a bola saiu da tela
         if (transform.position.x > limiteHorizontal || transform.position.x < -limiteHorizontal)
         {
