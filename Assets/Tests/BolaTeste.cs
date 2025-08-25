ef32c1507f7844aebc05a80e4efe8afcddc26665
Isabelle <isasoarez21@gmail.com>
2025-08-25 16:28:06 -0300

    test: adiciona unit tests para o BolaService

---
+++ Assets/Tests/BolaTeste.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class BolaTeste
+{
+   private BolaService _bolaService;
+
+    [SetUp]
+    public void Setup()
+    {
+        _bolaService = new BolaService();
+    }
+
+    [Test]
+    public void ChecaJogoIniciado_DeveRetornarCorreto()
+    {
+        var delay = 0f;
+        var jogoIniciado = false;
+
+        // Primeira vez → deve retornar true
+        Assert.IsTrue(_bolaService.ChecaJogoIniciado(delay, jogoIniciado));
+    }
+
+}
